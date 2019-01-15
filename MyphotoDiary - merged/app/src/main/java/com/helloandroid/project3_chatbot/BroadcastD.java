1b1f142321ad4f6a2aa546083298a626d8b73789
K-LeeJS <46184035+K-LeeJS@users.noreply.github.com>
2019-01-15 16:37:18 +0900

    01 15 4:37

---
+++ MyphotoDiary - merged/app/src/main/java/com/helloandroid/project3_chatbot/BroadcastD.java
@@ -0,0 +1,55 @@
+package com.helloandroid.project3_chatbot;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+public class BroadcastD extends BroadcastReceiver {
+    String INTENT_ACTION = Intent.ACTION_BOOT_COMPLETED;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {//알람 시간이 되었을때 onReceive를 호출함
+        //NotificationManager 안드로이드 상태바에 메세지를 던지기위한 서비스 불러오고
+
+        /*
+        NotificationManager notificationmanager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(R.drawable.on).setTicker("HETT").setWhen(System.currentTimeMillis())
+                .setNumber(1).setContentTitle("푸쉬 제목").setContentText("푸쉬내용")
+                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE).setContentIntent(pendingIntent).setAutoCancel(true);
+
+        notificationmanager.notify(1, builder.build());
+        */
+        /*
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("알림 제목");
+        builder.setContentText("알람 세부 텍스트");
+
+        builder.setColor(Color.RED);
+        // 사용자가 탭을 클릭하면 자동 제거
+        builder.setAutoCancel(true);
+
+        // 알림 표시
+
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+
+        // id값은
+        // 정의해야하는 각 알림의 고유한 int값
+        notificationManager.notify(1, builder.build());
+        */
+    }
+
+}
